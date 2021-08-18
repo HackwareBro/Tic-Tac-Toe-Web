3a3dc859096ded3cd6ae960b35162b688badc9c7
HackwareBro <hackwarebro@gmail.com>
2021-08-18 09:31:20 +0500

    Now project is in working condition. CPU mode and player mode both are in working
    bugs removed, now diagonal matching also supported to win. Some bugs removed in CPU mode algorithm. Readability of code and design of web will be improved by time to time.

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ function setMode(){
     mode = document.querySelector('input[name="mode"]:checked').value;
     game_started = true;
     document.getElementById('modebox').style.display = 'none';
+    for(let i = 0; i < 3; i++){     //Resetting everything
+        for(let j = 0; j < 3; j++){
+            document.getElementById(places[j][i]).className = 'empty';
+        }
+    }
+    document.getElementById('result').innerHTML = '';
 }
 
 function change(){
@@ -32,7 +38,9 @@ function cpuMode(){
         // cpu turn algo
         if(game_started == true){   // To stop CPU turn when player won the game
             cpu_turn = false;
-            while(cpu_turn == false){
+            //CheckDraw used avoid cpu from entering infinite loop because when there is no place to mark then 
+            // cpu might go to infinite loop without checkdraw()
+            while(cpu_turn == false && checkDraw() == false){ 
                 randomPlace = Math.trunc((Math.random() * 9) + 1) 
                 place = document.getElementById(randomPlace)
                 if(place.className == 'empty'){
@@ -76,5 +84,34 @@ function checkWin(player){
             break;
         }
     }
+    diagonalComplete = true;
+    oppositeDiagonalComplete = true;
+    for(let j = 0; j < 3; j++){
+        if(document.getElementById(places[j][j]).className != player){
+            diagonalComplete = false;
+        }
+        if(document.getElementById(places[j][2-j]).className != player){
+            oppositeDiagonalComplete = false;
+        }
+    }
+    if(diagonalComplete == true || oppositeDiagonalComplete == true){
+        document.getElementById('result').innerHTML = player +' won';
+        game_started = false;
+        document.getElementById('modebox').style.display = 'block';
+    }
 }
-
+function checkDraw(){   // to check that all the places are filled
+    noPlace = true;
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            if(document.getElementById(places[j][i]).className == 'empty'){
+                noPlace = false;
+                return noPlace;
+            }
+        }
+    }
+    document.getElementById('result').innerHTML = "Game Draw";
+    game_started = false;
+    document.getElementById('modebox').style.display = 'block';
+    return noPlace;
+}
